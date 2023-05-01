aba9f118030c3ff7fde9382ee26b44e03d877f4b
BleachG <122334045+BleachG@users.noreply.github.com>
2023-05-01 18:39:54 +0800

    Add files via upload

---
+++ 计图大作业/实验1View.cpp
@@ -0,0 +1,128 @@
+﻿
+// 实验1View.cpp: C实验1View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "实验1.h"
+#endif
+
+#include "实验1Doc.h"
+#include "实验1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C实验1View
+
+IMPLEMENT_DYNCREATE(C实验1View, CView)
+
+BEGIN_MESSAGE_MAP(C实验1View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &C实验1View::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// C实验1View 构造/析构
+
+C实验1View::C实验1View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C实验1View::~C实验1View()
+{
+}
+
+BOOL C实验1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C实验1View 绘图
+
+void C实验1View::OnDraw(CDC* /*pDC*/)
+{
+	C实验1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// C实验1View 打印
+
+
+void C实验1View::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL C实验1View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void C实验1View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void C实验1View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void C实验1View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void C实验1View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// C实验1View 诊断
+
+#ifdef _DEBUG
+void C实验1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C实验1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C实验1Doc* C实验1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C实验1Doc)));
+	return (C实验1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C实验1View 消息处理程序
